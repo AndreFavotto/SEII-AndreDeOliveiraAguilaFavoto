2c6311e9c514d2a961d62a58006640287284e61f
andrefavotto <andrefavotto@hotmail.com>
2022-05-29 11:31:58 -0300

    Push Activity 02

--- SEMANA04/py02.py
+++ SEMANA04/py02.py
@@ -1,27 +1,25 @@
 ''' Exercício referente ao vídeo 02 
  André de Oliveira Águila Favoto - 11811EAU013''' 
 
-
-
-#print comum
+#common print
 """ message = "Hello World"
  
 print(message)
  """
 
-# usando ' 
+# using ' 
 
 """ message = "Bobby's World"
  
 print(message)
  """
-#print de multiplas linhas
+#multiple line print
 ''' message = """Bobby's World is
 nothing but fake"""
 
 print(message) '''
 
-#funcao len && acessando caracteres especificos ou intervalos
+#len function and accessing individual characters/intervals
 """ message = "Hello World"
  
 print(len(message))
@@ -29,10 +27,22 @@ print(message[1])
 print(message[:5])
  """
 
-#funcao len && acessando caracteres especificos ou intervalos
+#useful string methods
 """ message = "Hello World"
+
  
-print(len(message))
-print(message[1])
-print(message[:5])
+print(message.lower())
+print(message.upper())
+print(message.count('l'))
+print(message.count('Hello'))
+print(message.find('World'))
+aux_message = message.replace('World', 'Universe')
+print(aux_message)
  """
+
+#concatenating methods
+greeting = "Hello"
+name = "Michael"
+message = greeting + ', ' + name + '!'
+print(message)
+print(f'{greeting}, {name}!')
