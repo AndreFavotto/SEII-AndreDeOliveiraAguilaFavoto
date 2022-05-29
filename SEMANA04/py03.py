0af347d1ea816697022dcd84a1b62340788ffe85
andrefavotto <andrefavotto@hotmail.com>
2022-05-29 12:12:39 -0300

    Push activity 03

--- SEMANA04/py03.py
+++ SEMANA04/py03.py
@@ -1,2 +1,30 @@
 ''' Exercício referente ao vídeo 03 
- André de Oliveira Águila Favoto - 11811EAU013''' 
+ André de Oliveira Águila Favoto - 11811EAU013'''
+ 
+#Identifying variable type
+""" num = 3.14
+print(type(num)) """
+
+#Arithmetic operators:
+""" print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2) 
+print(3 * (2 + 1))
+
+"""
+#some useful functions
+""" print(abs(-3))
+print(round(3.75, 1))
+num1 = int('100') #casting
+ """
+#Comparisons
+""" print(3 == 2)
+print(3 != 2)
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)
+print(3 <= 2) """
