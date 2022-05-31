b9edbf34614431e1d12edd86e93c3a1f2693b9d6
andrefavotto <andrefavotto@hotmail.com>
2022-05-31 19:09:13 -0300

    Push Activity 09

--- SEMANA04/py09.py
+++ SEMANA04/py09.py
@@ -1,2 +1,37 @@
 ''' Exercício referente ao vídeo 09 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+#My own module
+""" import module_py09 as my_module
+import sys
+
+if my_module.any_function:
+    print("Successfully excecuted imported module")
+else:
+    print("Error")
+
+print(sys.path) """
+
+#Random_number
+""" from random import randint, random
+
+random_number = randint(1,100)
+print(random_number) """
+
+#Math
+""" import math
+rads = math.radians(90)
+print(math.sin(rads)) """
+
+#Datetime and Calendar
+""" import datetime, calendar
+
+today = datetime.date.today()
+print(today)
+print(calendar.isleap(2020)) """
+
+#OS
+""" import os
+
+print(os.getcwd())
+print(os.__file__) """
