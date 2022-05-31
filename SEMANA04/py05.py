38783a77c8df1320cacee568b39389dd75f56719
andrefavotto <andrefavotto@hotmail.com>
2022-05-31 17:56:24 -0300

    Push Activity05

--- SEMANA04/py05.py
+++ SEMANA04/py05.py
@@ -1,2 +1,27 @@
 ''' Exercício referente ao vídeo 05 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+student = {'name': 'John', 'age': 25 , 'courses': ['Math', 'CompSci']}
+#Accessing dictionaries items
+"""
+print(student['name'])
+print(student.get('phone', 'Not Found'))
+"""
+#Updating
+"""
+student.update({'name': 'Jane', 'age': 26, 'phone': '555-5555'})
+print(student)
+ """
+
+ #Deleting
+""" del student['age']
+deleted = student.pop('name') """
+
+#Attributes
+""" print(len(student))
+print(student.keys())
+print(student.values())
+print(student.items())
+ """
+
+print(student)
