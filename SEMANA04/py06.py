97e812351d3e88bcb4283dbd3948e5e90f2901ab
andrefavotto <andrefavotto@hotmail.com>
2022-05-31 18:14:05 -0300

    Push Activity 06

--- SEMANA04/py06.py
+++ SEMANA04/py06.py
@@ -1,2 +1,31 @@
 ''' Exercício referente ao vídeo 06 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+name = 'andre'
+flag = 0
+
+#Conditionals
+""" if name == "andre" and flag == 1:
+    print('Andre logged')
+elif (name == 'felipe' or name == 'john') or not flag:
+    print('Admin logged')
+else:
+    print("no match")
+ """
+
+#Comparatives
+""" a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(id(a))
+print(id(b))
+print(a == b)
+print(a is b)
+ """
+
+"""Things that return false:
+False
+None
+Zero
+Empty sequences and dictionaries
+"""
---
+++ SEMANA04/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+if name == "andre" and flag == 1:
+    print('Andre logged')
+elif (name == 'felipe' or name == 'john') or not flag:
+    print('Admin logged')
+else:
+    print("no match")
