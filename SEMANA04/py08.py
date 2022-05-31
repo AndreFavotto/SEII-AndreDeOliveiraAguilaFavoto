d8f69623ab4704bf02816d0e4b0e33d1d8bad755
andrefavotto <andrefavotto@hotmail.com>
2022-05-31 18:43:13 -0300

    Push Activity 08

--- SEMANA04/py08.py
+++ SEMANA04/py08.py
@@ -1,2 +1,22 @@
 ''' Exercício referente ao vídeo 08 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+#Trying functions, returns, default values
+""" def hello_func(greeting, name = 'You'):
+    print('Hello Function\n')
+    return f'{greeting} {name}, function excecuted!'
+
+print(hello_func('Hello'))
+print(hello_func('Hello', 'Andre'))
+ """
+
+# *args and **kwargs
+""" def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+courses = ['Math', 'Art']
+info = {'name': 'John', 'age': 22}
+
+student_info(*courses, **info)
+ """
