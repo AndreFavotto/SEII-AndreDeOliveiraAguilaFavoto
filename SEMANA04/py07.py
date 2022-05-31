0af46f64cedd30de40308b20875816174385a0f3
andrefavotto <andrefavotto@hotmail.com>
2022-05-31 18:21:26 -0300

    Push Activity 06

--- SEMANA04/py07.py
+++ SEMANA04/py07.py
@@ -1,2 +1,33 @@
 ''' Exercício referente ao vídeo 07 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+nums = [1,2,3,4,5]
+
+#Break
+""" for num in nums:
+    if num == 3:
+        print('Found!')
+        break
+    print(num) """
+
+#Continue
+""" for num in nums:
+    if num == 3:
+        print('Found!')
+        continue
+    print(num) """
+
+#Nested loops
+""" for num in nums:
+    for letter in 'abc':
+        print(num, letter) """
+
+#Range
+""" for i in range(1,11):
+    print(i) """
+
+#While
+""" x = 0
+while x < 10:
+    print(x)
+    x+=1 """
