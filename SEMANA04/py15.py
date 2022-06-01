8ff2c956a2d621af3a4a71c989fe5387db616c48
andrefavotto <andrefavotto@hotmail.com>
2022-06-01 14:33:20 -0300

    Push Activity 15

--- SEMANA04/py15.py
+++ SEMANA04/py15.py
@@ -3,7 +3,7 @@
 
 #Manipulating csv file in format first_name,last_name,email
 
-import csv
+import csv, wget
 
 """ with open('csvfile.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -15,10 +15,14 @@ import csv
             csv_writer.writerow(line)
  """
 
-with open('csvfile.csv', 'r') as csv_file:
+url_csv = 'https://raw.githubusercontent.com/CoreyMSchafer/code_snippets/master/Python-CSV/names.csv'
+path_destination = './aux_files/'
+wget.download(url_csv, path_destination)
+
+with open('./aux_files/names.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file) #associates column title as key and each line as a value of the respective key
     
-    with open('new_names.csv', 'w') as new_file:
+    with open('./aux_files/new_names.csv', 'w') as new_file:
         fieldnames = ['first_name', 'last_name', 'email']
         
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
@@ -27,4 +31,3 @@ with open('csvfile.csv', 'r') as csv_file:
 
         for line in csv_reader:
             csv_writer.writerow(line)
-        
