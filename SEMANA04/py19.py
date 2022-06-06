a4cf712342ad6975d924e54c05c811f5c0ed985b
andrefavotto <andrefavotto@hotmail.com>
2022-06-06 20:46:15 -0300

    Push Activity 19

--- SEMANA04/py19.py
+++ SEMANA04/py19.py
@@ -1,2 +1,17 @@
 ''' Exercício referente ao vídeo 19 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+li = [9,1,8,2,7,3]
+"""
+s_li = sorted(li, reverse=True) #keeps the original one and returns the sorted
+
+print('Sorted list:\t', s_li)
+print('Original list:\t', li)
+
+# li.sort() #switchs the list in place and returns none
+
+ """
+""" student = {'name': 'John', 'age': 25 , 'courses': ['Math', 'CompSci']}
+s_student = sorted(student) #returns sorted keys
+print('Dict\t', s_student) """
+
