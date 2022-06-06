f498032679b4cc3072ba1b3071f89f6b9314e14e
andrefavotto <andrefavotto@hotmail.com>
2022-06-06 20:46:27 -0300

    Push Activity 20

--- SEMANA04/py20.py
+++ SEMANA04/py20.py
@@ -1,2 +1,18 @@
 ''' Exercício referente ao vídeo 20 
  André de Oliveira Águila Favoto - 11811EAU013''' 
+
+
+try:
+    f = open('./aux_files/testfile.txt')
+    # if f.name == 'unwanted_file.txt':
+    #     raise Exception
+except FileNotFoundError as e:
+    print(e)
+except Exception as e:
+    print(e)
+else:
+    print(f.read())
+    f.close()
+finally:
+    print('Executing finally')
+
