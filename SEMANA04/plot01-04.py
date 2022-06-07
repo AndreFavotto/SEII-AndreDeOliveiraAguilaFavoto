e57fd218bb5019e21c3edadfaf3b8deff83e64fe
andrefavotto <andrefavotto@hotmail.com>
2022-06-06 21:37:41 -0300

    Push Plot Activities

---
+++ SEMANA04/plot01-04.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(1,10,10)
+y = np.linspace(100,1,10)
+plt.scatter(x,y)
+plt.show()
---
+++ SEMANA04/plot05-07.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = np.arange(0,1000,1)
+plt.plot(x1,x1**2)
+plt.show()
